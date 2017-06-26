63f1d31ff2fd92dafd3eee683c78b02df47b0eb2
sumitganguly85 <sumitganguly85@gmail.com>
2017-06-27 01:03:49 +0530

    Add files via upload

---
+++ Tutorials_Link.sql
@@ -0,0 +1,13 @@
+--Tutorial Link
+
+--Hive ORC File Format Basic -->
+https://www.youtube.com/watch?v=yzZPe7O9oQM
+
+--Create Hive Table From JAVA
+https://www.tutorialspoint.com/hive/hive_create_table.htm
+
+--Hive Partitions
+http://hadooptutorial.info/partitioning-in-hive/
+
+--Hive Bucketing
+http://myitlearnings.com/bucketing-in-hive/
